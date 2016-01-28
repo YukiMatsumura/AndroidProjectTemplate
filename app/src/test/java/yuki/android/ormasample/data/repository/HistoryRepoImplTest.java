aabcbe83c098e253ce17f6c7f964175ebe15f008
MatsumuraYuki <define1394@gmail.com>
2016-01-29 00:37:39 +0900

    test(repository): add HistoryRepoImpl construct with null test case

--- app/src/test/java/yuki/android/ormasample/data/repository/HistoryRepoImplTest.java
+++ app/src/test/java/yuki/android/ormasample/data/repository/HistoryRepoImplTest.java
@@ -1,5 +1,7 @@
 package yuki.android.ormasample.data.repository;
 
+import android.test.suitebuilder.annotation.MediumTest;
+
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TestRule;
@@ -17,10 +19,17 @@ import yuki.android.ormasample.test.RobolectricConfig;
 
 @RunWith(CustomRobolectricTestRunner.class)
 @Config(sdk = RobolectricConfig.RUNNING_SDK_VERSION)
+@MediumTest
 public class HistoryRepoImplTest {
 
   @Rule
   public TestRule rule = new DefaultRobolectricRule();
+
+  @Test(expected = NullPointerException.class)
+  public void DI引数NULL() throws Exception {
+    new HistoryRepoImpl(null);
+  }
+
 
   @Test
   public void History情報の全件取得() throws Exception {
